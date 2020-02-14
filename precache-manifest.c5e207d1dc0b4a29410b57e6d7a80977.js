5f641c80230ca5d6cb3197d985fe191091c84baa
Jotham Hernandez <jotham_hernandez@hotmail.com>
2020-02-14 16:44:40 +0800

    updated master

---
+++ precache-manifest.c5e207d1dc0b4a29410b57e6d7a80977.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2fd756f8746e029c5f",
+    "url": "https://jothamhernandez.github.io/css/app.84ccbd79.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "https://jothamhernandez.github.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "942eb9cb3e95c52bd2a1094872c511f3",
+    "url": "https://jothamhernandez.github.io/index.html"
+  },
+  {
+    "revision": "712fdb318ccdb2201bf2",
+    "url": "https://jothamhernandez.github.io/js/about.22333c1c.js"
+  },
+  {
+    "revision": "5f2fd756f8746e029c5f",
+    "url": "https://jothamhernandez.github.io/js/app.083129b6.js"
+  },
+  {
+    "revision": "e1454f040b22c84893db",
+    "url": "https://jothamhernandez.github.io/js/chunk-vendors.e1dd45ab.js"
+  },
+  {
+    "revision": "4e8aaacc773d14e47855a764f3c8aa20",
+    "url": "https://jothamhernandez.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://jothamhernandez.github.io/robots.txt"
+  }
+]);
