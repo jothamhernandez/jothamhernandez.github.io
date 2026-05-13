f6287034e2c1dea74fbe7f900848154ca9c181e9
Jotham Hernandez <jotham_herandnez@hotmail.com>
2026-05-13 14:04:55 +0900

    updated analytics

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 import Landing from './components/Landing.vue'
+
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-LMPTJSNQ9T',
+      async: true
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LMPTJSNQ9T');
+      `,
+      type: 'text/javascript'
+    }
+  ]
+})
 </script>
 
 <template>
